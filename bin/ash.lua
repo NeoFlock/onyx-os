0a30e45b7ea6f49d64aefe90f54964d0beb51563
IonutParau <ionut.alex.parau@gmail.com>
2025-10-06 21:05:51 +0200

    made it idiot proof

--- bin/ash.lua
+++ bin/ash.lua
@@ -6,8 +6,10 @@ print("\x1b[36mAsh\x1b[32m v0.0.1\x1b[0m")
 while true do
 	local cwd = assert(k.chdir("."))
 	k.write(1, string.format("\x1b[36mroot\x1b[0m@\x1b[33m%s\x1b[0m ", k.hostname()))
+	k.write(1, "\x1b[32m")
 	k.write(1, cwd)
 	k.write(1, " > ")
+	k.write(1, "\x1b[0m")
 	local line = readline()
 	if line then
 		local args = string.split(line:sub(1, -2), " ")
