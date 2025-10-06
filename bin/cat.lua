382096ded4f230ab7540b7695dc1bf81d2843af8
IonutParau <ionut.alex.parau@gmail.com>
2025-10-06 23:49:33 +0200

    cat program

---
+++ bin/cat.lua
@@ -0,0 +1,20 @@
+--!lua
+
+local paths = {...}
+
+local blockSize = 4096
+
+for _, path in ipairs(paths) do
+	local f = assert(k.open(path, "r"))
+	while true do
+		local data, err = k.read(f, blockSize)
+		if err then
+			k.close(f)
+			error(err)
+		end
+		if not data then break end
+		k.write(1, data)
+		coroutine.yield()
+	end
+	k.close(f)
+end
